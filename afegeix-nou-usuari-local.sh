bab56a63519aae0141ed91f018851d407bfe5199
Josue Zambrano <a18joszamper@inspedralbes.cat>
2019-11-13 20:45:32 +0100

    usage

--- afegeix-nou-usuari-local.sh
+++ afegeix-nou-usuari-local.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+usage() {
+        echo "Usage: ${0} USERNAME [FULLNAME|COMMENT]" 1>&2; exit 1;
+}
+
+while getopts ":u:f" OPTION; do
+        case "${OPTION}" in
+                u)
+                        USERNAME=${OPTARG}
+                        ;;
+                f)
+                        FULLNAME=${OPTARG}
+                        ;;
+                \?)
+                        usage
+                        ;;
+                :)
+                        usage
+                        ;;
+        esac
+done
+
+#shift "$(( OPTIND - 1 ))"
+
+
 if [[ "${UID}" -eq 0 ]]
 then
-	if [ -z "$1" ]
-	then	
-		if [ -z "$2" ]
-			useradd -c "$2" -m "$1"
-		else
-			useradd -m "$1"
-	fi
+        if [ -z "$USERNAME" ]
+        then
+                if [ -z "$FULLNAME" ]
+                then
+                        useradd -c "$FULLNAME" -m "$USERNAME"
+                else
+                        useradd -m "$USERNAME"
+                fi
+        else
+                usage
+        fi
 
 else
-	echo 'No tienes los permisos necesarios.'
+        echo 'No tienes los permisos necesarios.'
 fi
+
