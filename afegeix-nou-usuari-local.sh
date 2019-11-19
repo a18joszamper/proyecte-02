a3d917ed880a2aa125efc326150cf14f8a90351f
a18joszamper <a18joszamper@inspedralbes.cat>
2019-11-19 18:21:44 +0100

    Back

--- afegeix-nou-usuari-local.sh
+++ afegeix-nou-usuari-local.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
 usage() {
-        echo "Usage: ${0} USERNAME [FULLNAME|COMMENT]" 1>&2; exit 1;
+        echo "Usage:"
+	echo "${0} [-u <USERNAME>] [-f <FULLNAME>|<COMMENT>]" 1>&2; exit 1;
 }
 
-while getopts ":u:f" OPTION; do
-        case "${OPTION}" in
+
+while getopts ':u:f' o; do
+        case "${o}" in
                 u)
                         USERNAME=${OPTARG}
                         ;;
                 f)
                         FULLNAME=${OPTARG}
                         ;;
-                \?)
-                        usage
-                        ;;
                 :)
+			echo "ERROR: Option -$OPTARG requires an argument"
+			usage
+                        ;;
+                \?)
+
+			echo "ERROR: Invalid option -$OPTARG"
                         usage
                         ;;
         esac
 done
 
-#shift "$(( OPTIND - 1 ))"
+shift $((OPTIND-1))
 
+PASSWORD=`openssl rand -base64 15`
 
 if [[ "${UID}" -eq 0 ]]
 then
-        if [ -z "$USERNAME" ]
+        if [ ! -z "$USERNAME" ]
         then
                 if [ -z "$FULLNAME" ]
                 then
-                        useradd -c "$FULLNAME" -m "$USERNAME"
+                        useradd -m "$USERNAME" -p "$PASSWORD"
                 else
-                        useradd -m "$USERNAME"
+                        useradd -c "$FULLNAME" -m "$USERNAME" -p "$PASSWORD"
                 fi
-        else
-                usage
+		echo $USERNAME:$PASSWORD | chpasswd
+		echo "La cuenta ha sido creada."
+		echo "El usuario es: $USERNAME"
+		echo "La contraseña: $PASSWORD"
+		echo "El hostname es: `hostname`"
+	else
+		usage
         fi
 
 else
